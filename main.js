28c54867a77d52f69d417969e5c7948d6e30dc03
sivaaravindh <sivaaravind262003@gmail.com>
2022-12-15 08:59:16 -0800

    index

---
+++ main.js
@@ -0,0 +1,11 @@
+let a=parseInt (prompt("Enter the value"));
+
+for (var x=1; x<=a; x++) {
+    
+
+         if (x % 2 != 0) {
+            document.write(x + " is odd"+ "<br>");   
+    }
+    
+}
+ 
